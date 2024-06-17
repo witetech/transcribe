0612557dde6d07cc3bd4de5cb4cb8f3aaed579b4
Ömer Karaca <omerkaracca@gmail.com>
2024-06-17 17:12:57 +0300

    Add Splash Screen & App Icon

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 // Project imports:
 import 'package:transcribe/firebase.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
   await dotenv.load(fileName: ".env");
   await initializeFirebase();
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
@@ -22,7 +25,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Transcribe',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.black,
+        ),
         useMaterial3: true,
       ),
       home: const Placeholder(),
